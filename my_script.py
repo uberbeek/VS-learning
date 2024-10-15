6ece3dca26abb22d852c66a3361f0ed9d9699231
uberbeek <uberbeek@gmail.com>
2024-10-15 06:12:54 -0700

    I used a formatter to help reformat

--- my_script.py
+++ my_script.py
@@ -2,5 +2,13 @@ import os
 
 
 print(f'my working directory is {os.getcwd()}')
+
+x = 10
+
+if x % 2 == 0:
+    print(f'{x} is even')
+else:
+    print(f'{x} is odd')
+
 print("this is my script for learning how to use git in vs")
 print("I find GitHub to be incredibly frustrating")
